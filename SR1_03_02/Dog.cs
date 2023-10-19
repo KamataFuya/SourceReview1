940fc30f32be63341cdb6c2bb1caa5382873b1f1
FuyaKamata <k021g1486@g.neec.ac.jp>
2023-10-19 14:17:25 +0900

    クラスの要素 実装編

---
+++ SR1_03_02/Dog.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR1_03_02
+{
+    internal class Dog
+    {
+        // 状態や属性 「 データ 」 … フィールド
+        private bool hungryState;
+
+        // 振る舞い 「操作」 … メソッド
+        public Dog()
+        {
+            hungryState = true;
+        }
+
+        // 食べる → 満腹になる
+        public void Eat()
+        {
+            Console.WriteLine("ぱくぱく");
+            hungryState = false; // おなかはすいていない
+        }
+
+        // 走る!! → ハラペコ状態にする
+        public void Run()
+        {
+            Console.WriteLine("わんわんわん");
+            hungryState = true; //おなかがすいた
+        }
+
+        // おなかがすいている状態か調べる
+        public bool IsHungry()
+        {
+            return hungryState;
+        }
+    }
+}
---
+++ SR1_03_02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR1_03_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog pochi = new Dog();
+
+            // コンストラクタが働いているかチェック
+            // 最初の 空腹状態を表示してみる
+            Console.WriteLine("空腹状態 : {0}", pochi.IsHungry());
+
+            // pochiにエサを食べさせる
+            pochi.Eat();
+            Console.WriteLine("空腹状態 : {0}", pochi.IsHungry());
+
+            // pochiを走らせる
+            pochi.Run();
+            Console.WriteLine("空腹状態 : {0}", pochi.IsHungry());
+
+            //一時停止
+            Console.ReadLine();
+        }
+    }
+}
