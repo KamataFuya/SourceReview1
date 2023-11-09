daec16d5ba5c2ab568cc987ebb256c7704f26707
FuyaKamata <k021g1486@g.neec.ac.jp>
2023-11-09 16:10:23 +0900

    クラスの継承_特化_実装編

---
+++ SR1_06_02/FlyingRobot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR1_06_02
+{
+    internal class FlyingRobot : Robot
+    {
+        // メソッド
+        // コンストラクタ
+        public FlyingRobot(string name) : base(name)
+        {
+
+        }
+
+        // 爆弾を落とす
+        public override void Attack()
+        {
+            Console.WriteLine("{0}は、爆弾を落とした!", name);
+        }
+
+    }
+}
---
+++ SR1_06_02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR1_06_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Robot robot = new Robot("ロボット");
+            Console.WriteLine("名前 : {0}", robot.GetName());
+            robot.Attack();
+
+            FlyingRobot flyingRobot = new FlyingRobot("空飛ぶロボ");
+            Console.WriteLine("名前 : {0}", flyingRobot.GetName());
+            flyingRobot.Attack();
+
+            TankRobot tankRobot = new TankRobot("タンクロボ");
+            Console.WriteLine("名前 : {0}", tankRobot.GetName());
+            tankRobot.Attack();
+
+            //一時停止
+            Console.ReadLine();
+        }
+    }
+}
