21e76aaea846a1372377cec6e39713a2a9f5a3ad
FuyaKamata <k021g1486@g.neec.ac.jp>
2023-12-07 14:37:20 +0900

    クラス図からの実装 問題2

---
+++ SR1_09_02/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR1_09_02
+{
+    internal class Player
+    {
+        // フィールド
+        private Item item;
+
+        // メソッド
+        public Player(Item item)
+        {
+            this.item = item;
+        }
+
+        public void UseItem()
+        {
+            item.Use();
+        }
+    }
+}
---
+++ SR1_09_02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR1_09_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Playerクラスの実体を生成する際、Itemクラスの実体を渡す
+            Player player = new Player(new Item("こん棒"));
+
+            player.UseItem();
+
+            Console.ReadLine();
+        }
+    }
+}
